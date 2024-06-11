2744438d07bc6f60cb2c456dff8e43d2ad8fcedc
kaito <leonard.jay.cros@gmail.com>
2024-06-11 05:10:01 -0400

    updated fonts

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
 import Image from 'next/image';
-import { lusitana } from './ui/font';
+import { lusitana } from './ui/fonts';
 
 export default function Page() {
   return (
